60b4f98c6fac7398a4ab6c6e1069d4efe3800747
Gaurav <gauravlaxmanbhaitiwari@gmail.com>
2024-09-16 06:49:19 +0300

    ch-3_submission

---
+++ ch-3_Q-4_leap_year.py
@@ -0,0 +1,10 @@
+# Leap Year Checker Program
+
+# Ask the user to enter a year
+year = int(input("Enter a year: "))
+
+# Check if the year is a leap year
+if (year % 4 == 0 and year % 100 != 0) or (year % 400 == 0):
+    print(f"{year} is a leap year.")
+else:
+    print(f"{year} is not a leap year.")
